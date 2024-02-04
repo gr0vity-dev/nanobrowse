4ec8f71d4f81542703f084870bcd57bd7776d9c9
gr0vity-dev <gr0vity-dev@mail.void>
2024-02-04 23:18:40 +0100

    fix naming

--- nanobrowse/static/js/receivables.js
+++ nanobrowse/static/js/receivables.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const account = document.body.getAttribute("data-get-delegators-for-account");
     if (account) {
-        fetchRenderedData(`/receivables/${account}`, `#pendingTable tbody`).then(() => {            
-            hideElement("pendingSpinner"); 
+        fetchRenderedData(`/receivables/${account}`, `#receivableTable tbody`).then(() => {            
+            hideElement("receivableSpinner"); 
             handleCopyEvent(".copy_btn", "append");
             handleCopyEvent(".copy_btn_front", "prepend");
         });    
