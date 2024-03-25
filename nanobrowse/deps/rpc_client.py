6728381612f9c715f072173bd73a76a80a98d7cb
gr0vity-dev <gr0vity-dev@mail.void>
2024-03-25 17:14:23 +0100

    imp: remove os dependency and unused node_version

--- nanobrowse/deps/rpc_client.py
+++ nanobrowse/deps/rpc_client.py
@@ -1,20 +1,18 @@
 from nanorpc.client import NanoRpcTyped, NodeVersion
 from nanorpc.client_nanoto import NanoToRpcTyped
-import os
+from os import getenv
 
-RPC_URL = os.getenv("RPC_URL")
-AUTH_USERNAME = os.getenv("AUTH_USERNAME")
-AUTH_PASSWORD = os.getenv("AUTH_PASSWORD")
-NODE_VERSION = NodeVersion.V25_0
-NANO_TO_AUTH_KEY = os.getenv("NANO_TO_AUTH_KEY")  # optinal
+RPC_URL = getenv("RPC_URL")
+AUTH_USERNAME = getenv("AUTH_USERNAME")
+AUTH_PASSWORD = getenv("AUTH_PASSWORD")
+NANO_TO_AUTH_KEY = getenv("NANO_TO_AUTH_KEY")  # optinal
 
 
-def get_nanorpc_client(rpc_url=None, auth_username=None, auth_password=None, node_version=None):
+def get_nanorpc_client(rpc_url=None, auth_username=None, auth_password=None):
     # Set the environment variables if they don't exist and parameters are provided
     rpc_url = rpc_url or RPC_URL
     auth_username = auth_username or AUTH_USERNAME
     auth_password = auth_password or AUTH_PASSWORD
-    node_version = node_version or NODE_VERSION
 
     # Initialize and return the NanoRpc client
     return NanoRpcTyped(url=rpc_url,
