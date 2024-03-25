5c60a8b91393bbe30b01edf802df088ab83a691f
gr0vity-dev <gr0vity-dev@mail.void>
2024-03-25 16:09:08 +0100

    make KNOWN_REFRESH_INTERVAL configurable

--- nanobrowse/utils/constants.py
+++ nanobrowse/utils/constants.py
@@ -1,2 +1,5 @@
+from os import getenv
+
+
 KNOWN_ACCOUNTS_FILE = "/app/utils/known.json"
-KNOWN_REFRESH_INTERVAL = 600  # 10 minutes
+KNOWN_REFRESH_INTERVAL = getenv("KNOWN_REFRESH_INTERVAL") or 600  # 10 minutes
