f449b4510f48c5ad4c0d748e57db3b0e5e301a04
gr0vity-dev <85646666+gr0vity-dev@users.noreply.github.com>
2025-01-24 18:07:19 +0100

    Auto-format with BLACK formatter

--- nanobrowse/transformers/account.py
+++ nanobrowse/transformers/account.py
@@ -1,15 +1,21 @@
 from quart import Blueprint, jsonify, request, abort
 from deps.rpc_client import nanorpc
-from utils.formatting import get_time_ago, format_weight, format_balance, format_hash, format_account
+from utils.formatting import (
+    get_time_ago,
+    format_weight,
+    format_balance,
+    format_hash,
+    format_account,
+)
 from utils.rpc_execution import execute_and_handle_errors
 from utils.known import AccountLookup
 from utils.logger import logger
 
-account_transformer = Blueprint('account_transformer', __name__)
+account_transformer = Blueprint("account_transformer", __name__)
 account_lookup = AccountLookup()
 
 
-@account_transformer.route('/account/<account>', methods=['GET'])
+@account_transformer.route("/account/<account>", methods=["GET"])
 async def get_account_info(account):
     data = await fetch_account_info(account)
 
@@ -23,12 +29,19 @@ async def get_account_info(account):
 async def fetch_account_info(account):
 
     tasks = {
-        "account_info": nanorpc.account_info(account, include_confirmed="true", representative="true", receivable="true", weight="true")
+        "account_info": nanorpc.account_info(
+            account,
+            include_confirmed="true",
+            representative="true",
+            receivable="true",
+            weight="true",
+        ),
         "account_balance": nanorpc.account_balance(account),
     }
-    response = await execute_and_handle_errors(tasks, droppable_errors=["Account not found"])
+    response = await execute_and_handle_errors(
+        tasks, droppable_errors=["Account not found"]
+    )
     response["account"] = account
-
     response["account_info"]["receivable"] = response["account_balance"].get(
         "receivable"
     )
@@ -36,7 +49,7 @@ async def fetch_account_info(account):
 
 
 async def transform_account_data(data):
-    account_info = data.get('account_info', {})
+    account_info = data.get("account_info", {})
     main_account = data.get("account")
 
     receivable = account_info.get("receivable", 0)
@@ -44,31 +57,39 @@ async def transform_account_data(data):
     show_receivable = True if int(receivable) > 0 else False
 
     formatted_weight, weight_percent, show_weight = format_weight(
-        account_info.get("weight"))
+        account_info.get("weight")
+    )
 
     representative = account_info.get("representative")
-    is_known_account, known_account = await account_lookup.lookup_account(
-        main_account)
+    is_known_account, known_account = await account_lookup.lookup_account(main_account)
     is_known_representative, known_representative = await account_lookup.lookup_account(
-        representative)
+        representative
+    )
 
-    representative_formatted = known_representative["name"] if is_known_representative else format_account(
-        representative)
-    account_formatted = known_account["name"] if is_known_account else format_account(
-        main_account)
+    representative_formatted = (
+        known_representative["name"]
+        if is_known_representative
+        else format_account(representative)
+    )
+    account_formatted = (
+        known_account["name"] if is_known_account else format_account(main_account)
+    )
 
     response = {
         "account": main_account,
         "account_formatted": account_formatted,
         "is_known_account": is_known_account,
         "known_account": known_account,
-        "confirmed_balance": format_balance(account_info.get("confirmed_balance", 0), "any"),
+        "confirmed_balance": format_balance(
+            account_info.get("confirmed_balance", 0), "any"
+        ),
         "show_receivable": show_receivable,
         "receivable": receivable,
         "receivable_formatted": receivable_formatted,
         "block_count": account_info.get("block_count", 0),
         "confirmed_blocks": account_info.get("confirmed_height", 0),
-        "unconfirmed_blocks": int(account_info.get("block_count", 0)) - int(account_info.get("confirmed_height", 0)),
+        "unconfirmed_blocks": int(account_info.get("block_count", 0))
+        - int(account_info.get("confirmed_height", 0)),
         "frontier": account_info.get("confirmed_frontier"),
         "frontier_formatted": format_hash(account_info.get("confirmed_frontier")),
         "open_block": account_info.get("open_block"),
@@ -82,11 +103,13 @@ async def transform_account_data(data):
         "weight_formatted": formatted_weight,
     }
 
-    response["show_receivable"] = True if int(
-        response["receivable"]) > 0 else False
-    response["show_unconfirmed_count"] = True if int(
-        response["unconfirmed_blocks"]) > 0 else False
+    response["show_receivable"] = True if int(response["receivable"]) > 0 else False
+    response["show_unconfirmed_count"] = (
+        True if int(response["unconfirmed_blocks"]) > 0 else False
+    )
     response["show_weight"] = show_weight
-    response["is_pr"] = True if response["show_weight"] and weight_percent > 0.1 else False
+    response["is_pr"] = (
+        True if response["show_weight"] and weight_percent > 0.1 else False
+    )
 
     return response
